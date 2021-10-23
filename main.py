4d344efae3f736eec853e38368a979a85a604be6
rooyca <rooyca@localhost.localdomain>
2021-10-23 14:46:51 -0400

    🏺 Tyche v1.0

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-'''
-You can checkout the CC module here: https://github.com/naijopkr/credit-card-generator
-And the english dictionary here: https://github.com/matthewreagan/WebstersEnglishDictionary
-'''
+"""
+
+MIT License
+
+Copyright (c) 2021 Rooyca
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
 
 import os
 import rcc
@@ -121,9 +142,7 @@ async def reading_inbox(email: str):
 		username = email.split("@")[0]
 		domain = email.split("@")[1]
 		getting_inbox = requests.get(url, f"action=getMessages&login={username}&domain={domain}")
-		print(getting_inbox.json())
 		return {"info":getting_inbox.json()}
-
 
 @app.get('/v1/random/color')
 async def random_color(numb: int = 1, using: str = "hex"):
